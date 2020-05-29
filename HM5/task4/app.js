0bc586597b35db92518a9cae91c903a508b0364c
aleks.kositsyn <aleks.kositsyn@gmail.com>
2020-05-29 17:59:03 +0300

    add task4

---
+++ HM5/task4/app.js
@@ -0,0 +1,8 @@
+'use strict';
+
+let btns = document.querySelectorAll('button');
+let img = document.querySelectorAll('img');
+
+btns.forEach(function(button){
+    button.addEventListener('click', )
+})
